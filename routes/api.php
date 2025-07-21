7d3fb5926b4f3851fb461dc0ebc3f5fb544365ff
krekas <krekas@gmail.com>
2025-07-21 17:29:05 +0300

    auth

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('register', RegisterController::class);
+Route::post('login', LoginController::class);
 
 Route::get('categories', [CategoryController::class, 'index']);
 Route::get('products', [ProductController::class, 'index']);
